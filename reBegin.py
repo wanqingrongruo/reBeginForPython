9e26f4119537355e38463ca10725f03cd53d0702
ronizheng <devzheng@qq.com>
2017-12-01 17:59:36 +0800

    off work

--- reBegin.py
+++ reBegin.py
@@ -1,24 +1,29 @@
 import collections
 
+
 Card = collections.namedtuple('Card', ['rank', 'suit'])
+
 
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs heart'.split()
 
     def __init__(self):
-        self._cards =  [Card(rank, suit) for suit in self.suits
-                                         for rank in self.ranks]
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+
     def __len__(self):
         return len(self._cards)
+
     def __getitem__(self, position):
         return self._cards[position]
 
+
 # 列表推导使用原则: 只用列表推导来创建新的列表,并且尽量保持简短
-codes = [i*i for i in range(1, 10)]
+codes = [i * i for i in range(1, 10)]
 print(codes)
 symbols = '@#$%$$'
-beyond_ascii =  [ord(s) for s in symbols if ord(s) > 35]
+beyond_ascii = [ord(s) for s in symbols if ord(s) > 35]
 print(beyond_ascii)
 b_a_02 = list(filter(lambda c: c > 35, map(ord, symbols)))
 print(b_a_02)
@@ -32,6 +37,7 @@ print(tshirts)
 tu = tuple(ord(s) for s in symbols)
 print(tu)
 import array
+
 a = array.array('I', (ord(s) for s in symbols))
 print(a)
 
@@ -43,28 +49,31 @@ for passport in sorted(traveler_ids):
 ## 平行赋值
 a = 3
 b = 4
-b,a = a,b
+b, a = a, b
 print('a=%s,b=%s' % (a, b))
 tt = (20, 8)
 ## *运算符把一个可迭代的对象拆开作为一个函数的参数
 print(divmod(*tt))
 
 import os
+
 _, lastFileName = os.path.split('/Users/roni/Desktop/python/rebegin')
 print('lastFileName: %s' % lastFileName)
 ## * 表示剩下的元素
 c, d, *rest = range(5)
 print(c, d, rest)
 ## 嵌套元组拆包
-metro_areas = [('Tokyo', 'JP', (35.689733, 139.691667)), ('Tokyo1', 'JP', (35.689733, 139.691667)), ('Tokyo2', 'JP', (35.689733, 139.691667)), ('Tokyo3', 'JP', (35.689733, 139.691667))]
+metro_areas = [('Tokyo', 'JP', (35.689733, 139.691667)), ('Tokyo1', 'JP', (35.689733, 139.691667)),
+               ('Tokyo2', 'JP', (35.689733, 139.691667)), ('Tokyo3', 'JP', (35.689733, 139.691667))]
 print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
-fmt = '{:15} | {:9.6f} | {:9.6f}' # 格式化
+fmt = '{:15} | {:9.6f} | {:9.6f}'  # 格式化
 for name, cc, (latitude, longitude) in metro_areas:
     if longitude >= 0:
         print(fmt.format(name, latitude, longitude))
 
 ## 具名元组
 from collections import namedtuple
+
 ### 创建一个具名数组需要两个参数, 一个是类名,,另一个是类的各个字段,,
 City = namedtuple('City', 'name country population coordinates')
 tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
@@ -92,8 +101,8 @@ del l[5:7]
 l[3::2] = [11, 22]
 print(l)
 # 对序列使用+和* => 产生新序列, 不修改原有的序列
-print(l+l)
-print(l*3)
+print(l + l)
+print(l * 3)
 ## 建立由列表组成的列表 => 二维???
 board = [['_'] * 3 for i in range(3)]
 print(board)
@@ -112,7 +121,7 @@ print(id(ll))
 tt = (1, 2, 3)
 itt = id(tt)
 print(itt)
-tt *= 2 # 不可变序列创建新变量
+tt *= 2  # 不可变序列创建新变量
 print(tt)
 print(id(tt))
 
@@ -124,7 +133,7 @@ print(id(tt))
 
 # list.sort 方法和内置函数 sorted
 print(l.sort())  # 返回值是 none
-print(sorted(l)) # 有返回值,可以接受任何形式的可迭代对象做参数
+print(sorted(l))  # 有返回值,可以接受任何形式的可迭代对象做参数
 
 # 用 bisect 来管理已排序的序列  bisect模块包含 bisect和 insort 两个主要函数,都是利用二分查找算法在有序序列中查找或插入元素
 ## 用 bisect 来搜索
@@ -152,7 +161,7 @@ print(sorted(l)) # 有返回值,可以接受任何形式的可迭代对象做参
 ## bisect.insort
 # import  bisect
 # import  random
-# 
+#
 # SIZE = 7
 # random.seed(1729)
 # my_list = []
@@ -160,3 +169,25 @@ print(sorted(l)) # 有返回值,可以接受任何形式的可迭代对象做参
 #     new_item = random.randrange(SIZE*2)
 #     bisect.insort(my_list, new_item)
 #     print('%2d ->' % new_item, my_list)
+
+# 数组
+from array import array
+from random import random
+
+floats = array('d', (random() for i in range(10 ** 7)))  # 'd' 是类型码
+print(floats[-1])
+fp = open('floats.bin', 'wb')
+floats.tofile(fp)
+fp.close()
+floats2 = array('d')
+fp = open('floats.bin', 'rb')
+floats2.fromfile(fp, 10 ** 7)
+fp.close()
+print(floats2[-1])
+print(floats == floats2)
+
+## memoryView 内存视图
+numbers = array('h', [-2, -1, 0, 1, 2])
+memv = memoryview(numbers)
+print(len(memv))
+print(memv[0])
