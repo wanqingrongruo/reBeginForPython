a35ad23981d58efd41a85b0160d3b474f26af7bf
ronizheng <devzheng@qq.com>
2017-12-06 18:05:23 +0800

    3.6

--- DS3.0.py
+++ DS3.0.py
@@ -17,3 +17,52 @@ code_country = {code: country.upper() for code, country in DIAL_CODES}
 print(code_country)
 
 ### 常见的映射方法
+import sys
+import re
+
+WORD_RE = re.compile(r'\w+')
+index = {}
+with open(sys.argv[0], encoding='utf-8') as fp:
+    for line_no, line in enumerate(fp,1):
+        for match in WORD_RE.finditer(line):
+            word = match.group()
+            column_no = match.start() + 1
+            location = (line_no, column_no)
+            # occurences = index.get(word, [])
+            # occurences.append(location)
+            # index[word] = occurences
+            index.setdefault(word, []).append(location) # setdefault 处理找不到的键
+for word in sorted(index, key=str.upper):
+    print(word, index[word])
+
+### 映射的弹性键查询
+#### defaultdict
+from collections import defaultdict
+dd = defaultdict(list)
+
+class StrKeyDic(dict):
+    def __missing__(self, key):
+        if isinstance(key, str):
+            raise KeyError(key)
+        return  self[str(key)]
+    def __get__(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+    def __contains__(self, item):
+        return item in self.keys() or str(item) in self.keys()
+
+### 子类化 UserDict
+from  collections import UserDict
+
+class StrKeyDic00(UserDict):
+    def __missing__(self, key):
+        if isinstance(key, str):
+            raise KeyError(key)
+        return self[str(key)]
+    def __contains__(self, item):
+        return str(item) in self.data
+    def __setitem__(self, key, value):
+        self.data[str(key)] = value
+
